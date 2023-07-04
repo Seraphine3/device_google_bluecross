f49cb60bf491744d9230b4d01f49fe1b16635473
Seraphine3 <seraphine312@gmail.com>
2023-07-05 06:29:28 +0800

    b1c1: Initialize CherishOS 13

--- cherish_blueline.mk
+++ cherish_blueline.mk
@@ -1,30 +1,42 @@
 #
-# Copyright (C) 2018-2021 The LineageOS Project
+# Copyright (C) 2018-2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Evolution X stuff.
+# Inherit some common CherishOS stuff.
 TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
-TARGET_IS_PIXEL := true
-TARGET_BOOT_ANIMATION_RES := 1080
+# CherishOS Stuff with GApps
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_ENABLE_BLUR := true
+WITH_GMS := true
+TARGET_IS_PIXEL := true
+
+# Target Resolution 
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit device configuration
 $(call inherit-product, device/google/crosshatch/aosp_blueline.mk)
 
-include device/google/crosshatch/blueline/device-evolution.mk
-
 # Device identifier. This must come after all inclusions
-PRODUCT_BRAND := google
+PRODUCT_BRAND := Google
 PRODUCT_MODEL := Pixel 3
-PRODUCT_NAME := evolution_blueline
+PRODUCT_NAME := cherish_blueline
+
+PRODUCT_GMS_CLIENTED_BASE := android-google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=blueline \
     PRIVATE_BUILD_DESC="blueline-user 12 SP1A.210812.016.C2 8618562 release-keys"
+
+# Maintainer Stuff
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer= lunaire
+CHERISH_BUILD_TYPE := UNOFFICIAL 
 
 BUILD_FINGERPRINT := google/blueline/blueline:12/SP1A.210812.016.C2/8618562:user/release-keys
 
--- cherish_crosshatch.mk
+++ cherish_crosshatch.mk
@@ -1,30 +1,42 @@
 #
-# Copyright (C) 2018-2021 The LineageOS Project
+# Copyright (C) 2018-2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Evolution X stuff.
+# Inherit some common CherishOS stuff.
 TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
-TARGET_IS_PIXEL := true
-TARGET_BOOT_ANIMATION_RES := 1440
+# CherishOS Stuff with GApps
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_ENABLE_BLUR := true
+WITH_GMS := true
+TARGET_IS_PIXEL := true
+
+# Target Resolution 
+TARGET_BOOT_ANIMATION_RES := 1440
 
 # Inherit device configuration
 $(call inherit-product, device/google/crosshatch/aosp_crosshatch.mk)
 
-include device/google/crosshatch/crosshatch/device-evolution.mk
-
 # Device identifier. This must come after all inclusions
-PRODUCT_BRAND := google
+PRODUCT_BRAND := Google
 PRODUCT_MODEL := Pixel 3 XL
-PRODUCT_NAME := evolution_crosshatch
+PRODUCT_NAME := cherish_crosshatch
+
+PRODUCT_GMS_CLIENTED_BASE := android-google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=crosshatch \
     PRIVATE_BUILD_DESC="crosshatch-user 12 SP1A.210812.016.C2 8618562 release-keys"
+
+# Maintainer Stuff
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer=lunaire
+CHERISH_BUILD_TYPE := UNOFFICIAL 
 
 BUILD_FINGERPRINT := google/crosshatch/crosshatch:12/SP1A.210812.016.C2/8618562:user/release-keys
 
